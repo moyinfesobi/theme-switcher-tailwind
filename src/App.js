b319348ce74822b66966ad3e13ad037ff3b2217f
Moyinoluwa <moyinoluwafesobi7@gmail.com>
2023-11-27 14:20:10 +0100

    modify: modified theme switcher

--- src/App.js
+++ src/App.js
@@ -1,50 +1,25 @@
-import "./App.css";
+import React, {useState} from "react";
 
-function App() {
-  const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const themes = {
+  light : 'bg-white text-black',
+  dark : 'bg-gray-900 text-white',
+};
 
-function switchTheme(e) {
-    if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-      localStorage.setItem('theme', 'dark');
-    }
-    else {
-        document.documentElement.setAttribute('data-theme', 'light');
-      
-      localStorage.setItem('theme', 'light');
-    }    
-}
+const  App = ( ) => {
+   const [currrentTheme, setCurrentTheme] = useState("light");
 
-toggleSwitch.addEventListener('change', switchTheme, false);
+   const ToggleTheme = () => {
+         setCurrentTheme ((prevTheme) => (prevTheme === "light" ? "dark" : "light"))
+   };
   return (
-    <div className="App">
-      {/* Tailwind Theme switcher */}
-      <nav>
-        <div class="theme-switch-wrapper">
-          <label class="theme-switch" for="checkbox">
-            <input type="checkbox" id="checkbox" />
-            <div class="slider round"></div>
-          </label>
-          <em>Switch</em>
+    <div className={`min-h-screen ${themes[currrentTheme]}`}>
+        <div className="p-8">
+          <h1 className="text-3xl font-bold mb-4">Tailwind Theme Switcher</h1>
+          <button className="bg-blue-500 text-white px-4 py-2 rounded" onClick={ToggleTheme}>
+            Toggle Theme
+          </button>
+          <p className="mt-4"> Current Theme: {currrentTheme === "light" ? <span> Light </span> : <span> Dark </span>  }</p>
         </div>
-      </nav>
-      <section>
-        <article class="post">
-          <h1>What is lorem ipsum</h1>
-          <p>
-            {" "}
-            simply dummy text of the printing and typesetting industry. Lorem
-            Ipsum has been the industry's standard dummy text ever since the
-            1500s, when an unknown printer took a galley of type and scrambled
-            it to make a type specimen book. It has survived not only five
-            centuries, but also the leap into electronic typesetting, remaining
-            essentially unchanged. It was popularised in the 1960s with the
-            release of Letraset sheets containing Lorem Ipsum passages, and more
-            recently with desktop publishing software like Aldus PageMaker
-            including versions of Lorem Ipsum.
-          </p>
-        </article>
-      </section>
     </div>
   );
 }
