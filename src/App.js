532f24c89e6c63e43ec0be3b365b30d870437174
Moyinoluwa <moyinoluwafesobi7@gmail.com>
2024-01-10 16:31:55 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React, {useState} from "react";
+import NavigationIcon from "../src/images/navigation.svg";
+import StarIcon from "../src/images/star.svg";
+import MessangerIcon from "../src/images/messanger.svg";
+import GraphIcon from "../src/images/graph.svg";
+import EarthIcon from "../src/images/world.svg";
+import BuildingIcon from "../src/images/building.svg";
 
-const themes = {
-  light : 'bg-white text-black',
-  dark : 'bg-gray-900 text-white',
-};
+
 
 const  App = ( ) => {
-   const [currrentTheme, setCurrentTheme] = useState("light");
-
-   const ToggleTheme = () => {
-         setCurrentTheme ((prevTheme) => (prevTheme === "light" ? "dark" : "light"))
-   };
+ 
   return (
-    <div className={`min-h-screen ${themes[currrentTheme]}`}>
-        <div className="p-8">
-          <h1 className="text-3xl font-bold mb-4">Tailwind Theme Switcher</h1>
-          <button className="bg-blue-500 text-white px-4 py-2 rounded" onClick={ToggleTheme}>
-            Toggle Theme
-          </button>
-          <p className="mt-4"> Current Theme: {currrentTheme === "light" ? <span> Light </span> : <span> Dark </span>  }</p>
+    <div className="max-h-screen">
+        <div className="  h-full w-full ">
+          <div className="bg-[#5D5FEF] h-[84px] w-full flex justify-end p-5">
+           <div className="mr-10 text-white">
+           Header
+           </div>
+          </div>
+          <div className=" grid grid-cols-12 h-[100vh] w-full ">
+            <div className="col-span-1 bg-[#FBFCFF] grid justify-center border-r-[#EDEFF2] border-r-2">
+             <div className="pt-10"> 
+             <div className="bg-white rounded-full w-[50px] h-[50px] flex justify-center mb-[20px] drop-shadow-md" ><img width={30} height={30} src={NavigationIcon} alt="navigation" /> </div>
+             <div className="bg-white rounded-full w-[50px] h-[50px] flex justify-center mb-[20px] drop-shadow-md">  <img width={30} height={30} src={StarIcon} alt="navigation" /> </div>
+             <div className="bg-white rounded-full w-[50px] h-[50px] flex justify-center mb-[20px] drop-shadow-md"> <img width={30} height={30} src={MessangerIcon} alt="navigation" /> </div>
+             <div className="bg-white rounded-full w-[50px] h-[50px] flex justify-center mb-[20px] drop-shadow-md">  <img width={30} height={30} src={GraphIcon} alt="navigation" /> </div>
+             <div className="bg-white rounded-full w-[50px] h-[50px] flex justify-center mb-[20px] drop-shadow-md"> <img width={30} height={30} src={EarthIcon} alt="navigation" /> </div>
+             <div className="bg-white rounded-full w-[50px] h-[50px] flex justify-center mb-[20px] drop-shadow-md"> <img width={30} height={30} src={BuildingIcon} alt="navigation" /> </div>
+             </div>
+            </div>
+            <div className="col-span-11 mt-[63px] mx-[60px] bg-[#FBFCFF] "> 
+             {/* bg-[#FBFCFF] */}
+              <p className="text-[#131313] text-[36px] font-bold"> Welcome, VDM 👋 </p>
+              <p className="text-[#1F2105]"> You have no upcoming sessions</p>
+
+              <p className="text-[#131313] text-[22px] font-bold mt-5"> Meet your Preferred Mentor</p>
+              <p className="text-[#1F2105]"> Mentors auto-paired with you</p>
+            </div>
+          </div>
+        
         </div>
     </div>
   );
